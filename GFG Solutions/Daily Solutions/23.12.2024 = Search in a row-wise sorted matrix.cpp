ad47008ae17e9250096a1d36a503186d9f5b5ab2
Swaraj Das <swarajdas625w@gmail.com>
2024-12-23 09:39:53 +0530

    Added GFG daily

---
+++ GFG Solutions/Daily Solutions/23.12.2024 = Search in a row-wise sorted matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchRowMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n=mat.size();
+        int m=mat[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int l=0,h=mat[i].size()-1;
+            
+            while(l<=h)
+            {
+                int mid=l+(h-l)/2;
+                
+                if(mat[i][mid]==x) return true;
+                else if(mat[i][mid] > x) h=mid-1;
+                else
+                    l=mid+1;
+            }
+        }
+        
+        return false;
+    }
+};
