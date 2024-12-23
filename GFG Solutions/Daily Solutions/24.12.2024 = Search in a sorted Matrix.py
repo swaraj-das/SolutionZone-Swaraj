553d6ffd6f9287f8e8fe401197e045c3b8a4b53e
SWARAJ DAS <151845349+swaraj-das@users.noreply.github.com>
2024-12-24 00:06:03 +0530

    Create 24.12.2024 = Search in a sorted Matrix.py

---
+++ GFG Solutions/Daily Solutions/24.12.2024 = Search in a sorted Matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def searchMatrix(self,mat, x): 
+    	
+    	n = len(mat)
+    	m = len(mat[0])
+    	
+    	for i in range(n):
+    	    
+    	    for j in range(m):
+    	        
+    	        if mat[i][j] == x:
+    	            
+    	            return True
+    	
+    	return False
+    	
